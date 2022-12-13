626dafa1d3722ec093f0166d46cd8c990e0892b0
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2022-12-13 18:58:26 +0530

    Update indexOf_java.java

--- Advance other learn/indexOf_java.java
+++ Advance other learn/indexOf_java.java
@@ -1,26 +1,33 @@
 /******************************************************************************
-substring
-
+indexOf in Java
 *******************************************************************************/
+
 
 public class Main
 {
 	public static void main(String[] args) {
 	    
+	    
+	    // 0 indexing 
 	    String s = new String("This is Minded Code");
 	    String s1 = "Minded Code";
 	    
 	    int w = s.indexOf("is");
 	    int x = s1.indexOf("Code");
+	    int y = s.indexOf('M'); //8
 	    
 	    System.out.println("first:- " + w);
 		System.out.println("second:- " + x);
+		System.out.println("third:- " + y);
+		
 		
 	}
 }
+
 
 
 Output of the Program:
 
 first:- 2
 second:- 7
+third:- 8
