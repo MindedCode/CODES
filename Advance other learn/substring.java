fc7588592a1edf7f498df2e3a4a8322760d43e1f
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2022-12-13 18:07:33 +0530

    Create substring.java

---
+++ Advance other learn/substring.java
@@ -0,0 +1,17 @@
+/******************************************************************************
+substring
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	    String s = "Minded Code";
+		System.out.println(s.substring(7));
+	}
+}
+
+
+"Output of the program"
+  
+Code
