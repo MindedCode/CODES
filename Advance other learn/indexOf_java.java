0788d785a891580abd86b4a819bd2d58850e72d7
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2022-12-13 18:53:51 +0530

    Create indexOf_java.java

---
+++ Advance other learn/indexOf_java.java
@@ -0,0 +1,26 @@
+/******************************************************************************
+substring
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    String s = new String("This is Minded Code");
+	    String s1 = "Minded Code";
+	    
+	    int w = s.indexOf("is");
+	    int x = s1.indexOf("Code");
+	    
+	    System.out.println("first:- " + w);
+		System.out.println("second:- " + x);
+		
+	}
+}
+
+
+Output of the Program:
+
+first:- 2
+second:- 7
