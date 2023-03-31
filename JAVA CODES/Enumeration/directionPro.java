b23747a5032d9c71524d5b4556c1a079c2abc658
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2023-04-01 03:15:02 +0530

    Create directionPro.java
    Enumeration is defined as a list that  contains constants,
    is write the class type
    AND NO USE THE NEW OPERATOR TO INSTANCIATE IT.

---
+++ JAVA CODES/Enumeration/directionPro.java
@@ -0,0 +1,32 @@
+    /*
+    enumeration
+    */
+
+public class Main
+{
+    // create enumeration 
+    enum Direction{
+        East, West, North, South
+    }
+    
+    
+	public static void main(String[] args) {
+	    
+	    // Declare a variable of type of Direction (DateType-int, float, double)
+	   // and  Instantiate  the enum Direction (DtaTy)
+	   
+	    Direction dir = Direction.East;
+	    
+	   // but Instantiate the enum Direction
+	   // dir= Direction.East;
+	    
+		System.out.println("Value: " + dir);
+	}
+}
+
+
+
+
+"Output of the Program"
+
+//   Value: East;
