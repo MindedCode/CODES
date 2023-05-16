3cc6a9337c88b3358571e70b9df7fe01fbe3f558
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2023-05-16 13:59:45 +0530

    Create ceil.java

---
+++ JAVA CODES/Basic/ceil.java
@@ -0,0 +1,24 @@
+Example of Math.ceil()
+class Example{
+
+    public static void main(String args[]){ 
+        
+        double example1 = 8.24;
+        double example2 = 10.0;
+        int example3 = 2;
+        
+        double ceilOfExample1 = Math.ceil(example1); // ceil of 8.24 is 9.0
+        double ceilOfExample2 = Math.ceil(example2); // ceil of 10.0 is 10.0
+        double ceilOfExample3 = Math.ceil(example3); // ceil of 2 is 2.0
+        
+        System.out.println(ceilOfExample1); // prints 9.0
+        System.out.println(ceilOfExample2); // prints 10.0
+        System.out.println(ceilOfExample3); // prints 2.0
+    }
+}
+
+
+//Output of the Program:
+9.0
+10.0
+2.0
