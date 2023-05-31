a279770e3c81d330fa0da741352b6b61df1ddbd1
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2023-05-31 13:06:10 +0530

    Create if.java

---
+++ JAVA CODES/Basic/if.java
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+// 	    int i = 0;
+// 	    if(i)
+// 		System.out.println("Hello World");
+// 	}
+
+        // {
+	   // public static void main(String[] args) {
+	    boolean i =  true;
+	    if(i)
+		System.out.println("Hello World");
+	}
+}
+
+
+/*
+
+Error:
+Compilation failed due to following error(s).Main.java:13: error: incompatible types: int cannot be converted to boolean
+	    if(i)
+	       ^
+1 error*/
