72f9a2209d1709b7d08d8d940ca27e34360d0e43
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2023-07-25 13:38:37 +0530

    Update getch.c

--- c-code/placement/getch.c
+++ c-code/placement/getch.c
@@ -12,6 +12,11 @@ int main(){
     printf("\nEnter character\n");
     getch();
     printf("\nEnter character\n");
+
+
+    int b = getch();
+    int c = getch();
+    printf("age : %d%d", b, c);
     
 }
 
@@ -25,4 +30,5 @@ Hellow is 50
 
 Enter character
 
-Enter character
+Enter character age : 5051  // ye value char 2 and 1 ko liye hai  bs '2' and '1' character ko ascii language se convert kiya hai becoz humne display 
+    krne format specifier %d hi diya hai
