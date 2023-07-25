1d898b70a796cee7bd9fb69d814845b49036b11a
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2023-07-25 13:01:09 +0530

    Create getch.c

---
+++ c-code/placement/getch.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    // getch();
+    printf("Hellow\n");
+    int a = getch();
+    printf("\nEnter character\n");
+    getch();
+    printf("\nEnter character\n");
+    printf("Hellow is %d\n ", a);
+    printf("\nEnter character\n");
+    getch();
+    printf("\nEnter character\n");
+    
+}
+
+Output is:
+Hellow
+
+Enter character
+
+Enter character
+Hellow is 50
+
+Enter character
+
+Enter character
