ff36e0718f5942a63bf86f95419c21e1b6f94283
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2023-07-25 11:31:07 +0530

    Update Excape Squence.c

--- c-code/Escape Sequence/Excape Squence.c
+++ c-code/Escape Sequence/Excape Squence.c
@@ -22,4 +22,37 @@ Output is:======================================================================
 // Hello World@ ! 
 // / // \ \ \ \
 
+// ================================================Second program: ========================================
+
+
+main(){
+    printf("Hello World@ ! \n / // \\ \\ \\ \\ ///// \%%d %%d \\\\\\ " );    //firstly make pair then output single 
+}
+
+
+
+
+                         // some warning only below becoz no include the Header file or Preprocesive directive
+
+
+
+// main.c:1:1: warning: return type defaults to ‘int’ [-Wimplicit-int]
+//     1 | main(){
+//       | ^~~~
+// main.c: In function ‘main’:
+// main.c:2:5: warning: implicit declaration of function ‘printf’ [-Wimplicit-function-declaration]
+//     2 |     printf("Hello World@ ! \n / // \\ \\ \\ \\ ///// \%%d %%d \\\\\\ " );
+//       |     ^~~~~~
+// main.c:1:1: note: include ‘<stdio.h>’ or provide a declaration of ‘printf’
+//   +++ |+#include <stdio.h>
+//     1 | main(){
+// main.c:2:5: warning: incompatible implicit declaration of built-in function ‘printf’ [-Wbuiltin-declaration-mismatch]
+//     2 |     printf("Hello World@ ! \n / // \\ \\ \\ \\ ///// \%%d %%d \\\\\\ " );
+//       |     ^~~~~~
+
+Output is:========================================================================
+// main.c:2:5: note: include ‘<stdio.h>’ or provide a declaration of ‘printf’
+// Hello World@ ! 
+//  / // \ \ \ \ ///// %d %d \\\ 
+
 
