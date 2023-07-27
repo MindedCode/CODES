b4494894c1025b4a505f27707fd9713bb3c2ebe1
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2023-07-27 16:30:35 +0530

    Create void function and int function.c

---
+++ Weaird things/void function and int function.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void hello(){
+    // int a = 12;
+    printf("hello %d\n", 2);
+    
+}
+int helo(){
+    // int a = 12;
+    printf("hello %d", 3);
+    
+}
+int main()
+{
+    hello();
+    void hello();
+    helo();
+    int helo();
+
+    return 0;
+}
+
+
+Output is:
+
+hello 2
+hello 3
