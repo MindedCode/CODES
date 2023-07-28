c97a7204cdfdfe173c482570c0e34e46adef38de
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2023-07-28 10:55:03 +0530

    Update assignment 6.c

--- c-code/Assignment/Saurabh Shukala/assignment 6.c
+++ c-code/Assignment/Saurabh Shukala/assignment 6.c
@@ -3,6 +3,12 @@
                             1. print size of an int, a float, a char and a double type variable
                             2. find first digit of a given three digit number
                             3. find middle digit of a given three digit number
+			    4. last digit of a number stored in a variable as zero. (Example - if x=2345 then make it x=2340)
+        		5. input a number from the user and also input a digit. Append a digit in the number and print the resulting number.
+                		(Example - number=234 and digit=9 then the resulting number is 2349)
+                	6. Assume price of 1 USD is INR 76.23. Write a program to take the amount in INR and convert it into USD.
+            		
+	      		7. to take a three digit number from the user and rotate its digits by one position towards the right.
 
 *******************************************************************************/
 #include<stdio.h>
@@ -69,3 +75,29 @@ int main() {
 
     return 0;
 }
+
+/*****************************************************************************/
+
+#include<stdio.h>
+
+void onePosiRotate(int n){
+    int digit = n%10;  //4
+    int ans = ans + digit  * 1000;  //4 
+    n/=10;  //123
+    ans = ans + n;
+    printf("\nAns: %d", ans);
+    
+}
+int main(){
+    int number = 1234;
+    
+    
+    int digit = number/10;
+        
+    int ans = digit*10;
+    printf("%d", ans);
+    
+    onePosiRotate(number);
+}
+
+
