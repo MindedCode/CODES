8e7aedde829d8f58373e8cc62851d9cc4b7d46cd
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2023-08-21 15:58:14 +0530

    Update singleLoopUsingconditional.java

--- JAVA CODES/pattern/singleLoopUsingconditional.java
+++ JAVA CODES/pattern/singleLoopUsingconditional.java
@@ -22,4 +22,27 @@ public class Main {
 * * * * * 
 * * * * * * 
 * * * * * * * 
-* * * * * * * * 
+* * * * * * * *
+
+
+
+
+
+    public class Main {
+    public static void main(String[] args) {
+        int n = 4; // Number of rows
+        int count = 1;
+        int currentRow = 1;
+
+        while (count <= n * (n + 1) / 2) {
+            System.out.print("*");
+            count++;
+            
+            if (count > currentRow * (currentRow + 1) / 2) {
+                System.out.println();
+                currentRow++;
+            }
+        }
+    }
+}
+
