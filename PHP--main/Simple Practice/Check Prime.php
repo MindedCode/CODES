8515cd020e78d2ffb83980e940a4089a6a1acc14
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2023-11-04 18:33:20 +0530

    Create Check Prime or Not

---
+++ PHP--main/Simple Practice/Check Prime.php
@@ -0,0 +1,62 @@
+
+<!-- Hypertext Preprocessor. -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edited By Adarsh</title>
+</head>
+
+<body style="background-color: aqua;">
+    <!-- <center> -->
+    <h1 style="text-align:center; text-transform:uppercase;
+         color: blue; font-family: vardana; height: 5rem; padding-top: 2rem; ">
+        Welcome to My Coding
+    </h1>
+
+    <h2 style="background-color: green; padding: 1rem;">
+        <?php
+        // echo "Hii Ganesh";
+
+        //Check Prime or Not
+
+        // 1️⃣ Solution
+
+        
+        // if ($n <= 1) echo "Not a Prime Number";
+
+        // else {
+        //     $count = 0;
+        //     for ($i = 2; $i < $n; $i++) {
+        //         if ($n % $i == 0) {
+        //             $count++;
+        //         }
+        //     }
+        //     if ($count == 0) {
+        //         echo "Prime";
+        //     } else echo "Not Prime";
+        // }
+
+        // 2️⃣ Solution
+       
+        
+        // function isPrime($n){
+        //     if($n <=1) return false;
+        //     for($i=2; $i*$i <= $n; $i++){
+        //         if(!($n % $i)) return false;
+        //     }
+        //     return true;
+        // }
+
+        // $n = 7;
+        // $result = isPrime($n) ? "Yes Prime" : "Not Prime";
+        // echo "$result";
+
+        ?>
+    </h2>
+    <!-- </center> -->
+</body>
+
+</html>
