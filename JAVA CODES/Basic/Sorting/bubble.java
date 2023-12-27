978d2a4f8f95667e2a388720c8a273d8b80d67d9
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2023-12-28 04:23:54 +0530

    Update bubble.java

--- JAVA CODES/Basic/Sorting/bubble.java
+++ JAVA CODES/Basic/Sorting/bubble.java
@@ -11,7 +11,7 @@ public class Sorting
     static void bSort(int arr[]){
         for(int i=0; i<arr.length-1; i++){
             for(int j=0; j<arr.length-1-i; j++){
-                if(arr[j]>arr[j+1]){
+                if(arr[j]>arr[j+1]){  
                     // Swap
                     int t = arr[j];
                     arr[j] = arr[j+1];
