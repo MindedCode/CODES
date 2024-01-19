73967ab64fd80965ef35f278c25b47116af50ecf
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2024-01-19 19:43:12 +0530

    Update arr.java
    update the code and optimize the code only lines........

--- JAVA CODES/EachLoop/arr.java
+++ JAVA CODES/EachLoop/arr.java
@@ -1,3 +1,26 @@
+package day45;
+
+public class Even {
+    public static void main(String[] args) {
+
+        int arr[] = {1,2,3,4,5,6,5,};
+        int countOdd = 0;
+        int countEven = 0; 
+            
+        for (int i = 0; i < arr.length; i++) {
+            int temp = (i%2==0) ? countOdd++ : countEven++;
+        }
+        System.out.println(countOdd + " " + countEven);
+    }
+}
+
+
+
+
+
+
+
+
 int countOdd = 0 ;
         int countEven =  0 ;
        for(int i : arr){
